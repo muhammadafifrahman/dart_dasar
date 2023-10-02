846d2264d9852eec435a3b9a95ea9bb8f3baa623
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-03 06:45:35 +0800

    tugas if dan else

---
+++ bin/if_else.dart
@@ -0,0 +1,14 @@
+void main() {
+  int nilai = 25;
+  int nilai2 = 7;
+
+  if (nilai > 20 && nilai < 30) {
+    print("Nilai berada di antara 20 dan 30");
+  }
+
+  if (nilai2 % 2 == 0) {
+    print("Nilai adalah bilangan genap");
+  } else {
+    print("Nilai adalah bilangan ganjil");
+  }
+}
