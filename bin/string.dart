c2656ae6dece3950310d7c99af1795a64a6be9f4
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-02 10:41:31 +0800

    tugas string

--- bin/string.dart
+++ bin/string.dart
@@ -1,6 +1,13 @@
 void main() {
   String firstName = 'Muhammad';
   String lastName = 'Afif';
+  var name1 = firstName + lastName;
+  var name2 = 'Muhammad' 'Afif' 'Rahman';
+  var longString = '''
+  kalau string sangat panjang
+  sehingga sulit dibuat dalam satu baris
+  maka gunakan yang seperti ini
+  ''';
 
   print(firstName);
   print(lastName);
@@ -8,4 +15,9 @@ void main() {
   var fullName = '$firstName ${lastName}';
 
   print(fullName);
+
+  print(name1);
+  print(name2);
+
+  print(longString);
 }
