185b1a662ab1d1ffa279610599e9194d2461ced6
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-02 10:32:50 +0800

    tugas string

---
+++ bin/string.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = 'Muhammad';
+  String lastName = 'Afif';
+
+  print(firstName);
+  print(lastName);
+
+  var fullName = '$firstName ${lastName}';
+
+  print(fullName);
+}
