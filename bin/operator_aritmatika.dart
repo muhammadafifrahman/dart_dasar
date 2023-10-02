73e08d6b17adac0a82267eb2a3399ed0daf03e02
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-02 12:29:22 +0800

    tugas operator aritmatika

---
+++ bin/operator_aritmatika.dart
@@ -0,0 +1,13 @@
+void main() {
+  var hasil1 = 20 + 20;
+  var hasil2 = 50 - 20;
+  var hasil3 = 5 * 5;
+  var hasil4 = 10 / 5;
+  var hasil5 = 20 % 3 as int;
+
+  print(hasil1);
+  print(hasil2);
+  print(hasil3);
+  print(hasil4);
+  print(hasil5);
+}
