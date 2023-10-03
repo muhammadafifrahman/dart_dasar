e52e75b52d049b0710bfb82d949f89ade6e83dd5
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-04 06:46:00 +0800

    tugas if dan else

--- bin/if_else.dart
+++ bin/if_else.dart
@@ -1,6 +1,7 @@
 void main() {
   int nilai = 25;
   int nilai2 = 7;
+  int nilai3 = 23;
 
   if (nilai > 20 && nilai < 30) {
     print("Nilai berada di antara 20 dan 30");
@@ -10,5 +11,13 @@ void main() {
     print("Nilai adalah bilangan genap");
   } else {
     print("Nilai adalah bilangan ganjil");
+  }
+
+  if (nilai3 > 20) {
+    print("Nilai lebih besar dari 20");
+  } else if (nilai3 == 20) {
+    print("Nilai sama dengan 20");
+  } else {
+    print("Nilai kurang dari atau sama dengan 20");
   }
 }
