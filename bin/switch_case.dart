69c92150aa732388f157636f312454275ce94074
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-04 07:08:45 +0800

    tugas switch case

---
+++ bin/switch_case.dart
@@ -0,0 +1,18 @@
+void main() {
+  int nilai = 90;
+
+  switch (nilai) {
+    case 100:
+      print("Nilai anda adalah A");
+      break;
+    case 90:
+      print("Nilai anda adalah B");
+      break;
+    case 80:
+      print("Nilai anda adalah C");
+      break;
+    default:
+      print("Nilai anda adalah D");
+      break;
+  }
+}
