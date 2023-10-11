221aca4dcb99b7de4c886d354395a72194c077cc
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-11 08:46:37 +0800

    tugas optional parameter

--- bin/optional_parameter.dart
+++ bin/optional_parameter.dart
@@ -1,8 +1,17 @@
+// optional parameter
 void sayHello(String firstName, [String? lastName]) {
   print('Hello $firstName $lastName');
+}
+
+// default value
+void sayHalo(String namaPertama, [String namaTerakhir = '']) {
+  print('Hello $namaPertama $namaTerakhir');
 }
 
 void main() {
   sayHello('Budi');
   sayHello('Budi', 'Pratama');
+
+  sayHalo('Rudi');
+  sayHalo('Rudi', 'Khannedy');
 }
