cbe8bb5bcaf96ef2ace17a20378416d25be51b99
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-13 22:55:51 +0800

    tugas method expression body

---
+++ bin/oop/computer.dart
@@ -0,0 +1,8 @@
+// Method edxpression body
+class Computer {
+  void startup() => print("Computer is starting");
+
+  void shutdown() => print("computer is shutting down");
+
+  String getOperatingSystem() => "Linux";
+}
--- bin/oop/main.dart
+++ bin/oop/main.dart
@@ -1,9 +1,15 @@
+import 'computer.dart';
 import 'person.dart';
 
 void main() {
 // Memanggil Method
-var person = Person();
-person.name = "Eko Kurniawan";
-person.sayHello("Budi");
+  var person = Person();
+  person.name = "Eko Kurniawan";
+  person.sayHello("Budi");
 
+// Memanggil Method Expression Body
+  var computer = Computer();
+  computer.startup();
+  computer.shutdown();
+  print(computer.getOperatingSystem());
 }
