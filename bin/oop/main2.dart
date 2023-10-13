0d93e94196f2cc4be4c09c3572dff9a10fb6d587
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-14 03:26:15 +0800

    tugas super constructor

--- bin/oop/main2.dart
+++ bin/oop/main2.dart
@@ -1,7 +1,15 @@
+import 'manager2.dart';
 import 'person2.dart';
 
 void main() {
   // Mengakses Field Overriding
   var person = OtherPerson();
   person.sayHello("Eko");
+
+  // Menggunakan Super Constructor
+  var manager = Manager("Eko");
+  // manager.sayHello('Budi');
+
+  var vp = VicePresident('Kurniawan');
+  // vp.sayHello('Budi');
 }
