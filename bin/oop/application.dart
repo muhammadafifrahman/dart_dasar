55786f58cd407ba1e06d36cd076124b940cdbf78
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-10-17 23:49:17 +0800

    tugas static

---
+++ bin/oop/application.dart
@@ -0,0 +1,9 @@
+class Application {
+  static final String author = "Eko Kurniawan Khannedy";
+  static final String name = "Belajar Pemrograman Dart";
+}
+
+void main() {
+  print(Application.name);
+  print(Application.author);
+}
