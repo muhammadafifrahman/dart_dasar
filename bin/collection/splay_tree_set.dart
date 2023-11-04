c215593877704bf6348e5c74c8e6f80829a5e980
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-11-04 12:24:37 +0800

    tugas comparator

--- bin/collection/splay_tree_set.dart
+++ bin/collection/splay_tree_set.dart
@@ -1,7 +1,7 @@
 import 'dart:collection';
 
 void main() {
-  final treeSet = SplayTreeSet<int>();
+  final treeSet = SplayTreeSet<int>((first, second) => second.compareTo(first));
   treeSet.addAll([1, 6, 5, 4, 3, 2, 7, 8, 9]);
 
   print(treeSet);
