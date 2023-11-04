50d60aef4a4c0c8343aa4782ad80ff8fd758d0fc
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-11-04 18:19:25 +0800

    tugas check method

--- bin/collection/check_method.dart
+++ bin/collection/check_method.dart
@@ -1,6 +1,6 @@
 void main() {
   final numbers = [2, 4, 6, 8, 10];
 
-  print(numbers.any((element) => element > 11));
-  print(numbers.every((element) => element > 8));
+  print(numbers.any((element) => element > 5));
+  print(numbers.every((element) => element > 5));
 }
