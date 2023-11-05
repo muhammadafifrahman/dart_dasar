9cad00eb447b176cd9aa74f354c2c3450c29bb6a
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-11-05 15:29:00 +0800

    tugas set method

---
+++ bin/collection/set_method.dart
@@ -0,0 +1,8 @@
+void main() {
+  final names1 = {"Eko", "Kurniawan", "Khannedy"};
+  final names2 = {"Budi", "Kurniawan", "Nugraha"};
+
+  print(names1.union(names2));
+  print(names1.intersection(names2));
+  print(names1.difference(names2));
+}
