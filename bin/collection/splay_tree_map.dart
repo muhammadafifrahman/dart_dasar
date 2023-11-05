4eccadc8cd0bdd55f93678d21cf34c79d437db5d
Muhammad Afif Rahman <m.afifrahman07@gmail.com>
2023-11-05 16:49:34 +0800

    tugas splay tree map

---
+++ bin/collection/splay_tree_map.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main() {
+  final scores = SplayTreeMap<String, int>();
+
+  scores["Eko"] = 100;
+  scores["Budi"] = 100;
+  scores["Joko"] = 100;
+  scores["Dimas"] = 100;
+  scores["Donis"] = 100;
+
+  print(scores);
+}
